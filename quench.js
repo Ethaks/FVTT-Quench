0033dafe6b05c19c64bd6f2c57c42b0bf005f9e2
Cole Schultz <schultzcole@gmail.com>
2021-02-02 13:07:59 -0600

    Improve formatting

--- quench.js
+++ quench.js
@@ -33,15 +33,15 @@ quenchInit();
 class Quench {
     constructor(mocha, chai) {
         this.mocha = mocha;
-        this.chai = chai;
         this.mocha._cleanReferencesAfterRun = false;
+        this.chai = chai;
         this.suiteGroups = new Map();
         this.app = new QuenchResults(this);
     }
 
     registerSuiteGroup(name, fn, { displayName=null }={}) {
         if (this.suiteGroups.has(name)) {
-            ui.notifications.warn(`QUENCH: Suite group "${name}" already exists. Overwriting...`);
+            ui?.notifications?.warn(`QUENCH: Suite group "${name}" already exists. Overwriting...`);
         }
         this.suiteGroups.set(name, { displayName: displayName ?? name, fn });
         this.app.clear();
@@ -50,8 +50,7 @@ class Quench {
     async runSelectedSuiteGroups(groupKeys) {
         // Cleanup - create a new root suite and clear the state of the results application
         const Mocha = this.mocha.Mocha;
-        Mocha.suite = this.mocha.suite =  new Mocha.Suite("__root", new Mocha.Context(), true);
-
+        Mocha.suite = this.mocha.suite = new Mocha.Suite("__root", new Mocha.Context(), true);
         await this.app.clear();
 
         // Initialize mocha with a quench reporter
@@ -61,15 +60,14 @@ class Quench {
         });
 
         // Prepare context methods to be provided to test fixtures
-        const { after, afterEach, before, beforeEach, describe, it, utils } = this.mocha.Mocha;
+        const { after, afterEach, before, beforeEach, describe, it, utils } = Mocha;
         const { assert } = this.chai;
-
         const context = {
             after, afterEach, before, beforeEach, it, utils,
             assert,
-        }
+        };
 
-        // Register suites and tests for selected fixtures
+        // Register suites and tests for provided suite groups
         for (let key of groupKeys) {
             // Override `describe` to add a property to the resulting suite indicating which quench suite group the suite belongs to.
             context.describe = function quenchDescribe(...args) {
@@ -78,7 +76,7 @@ class Quench {
                 return suite;
             };
 
-            // Call the suite registration function
+            // Call the group registration function
             await this.suiteGroups.get(key).fn(context);
         }
 
