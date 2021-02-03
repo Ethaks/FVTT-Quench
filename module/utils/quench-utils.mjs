26ebf8bed78d8c915fda98e1398aa0fc53f5e585
Cole Schultz <schultzcole@gmail.com>
2021-02-03 11:48:34 -0600

    Add doc comments for quench-utils.mjs

--- module/utils/quench-utils.mjs
+++ module/utils/quench-utils.mjs
@@ -1,3 +1,8 @@
+/**
+ * Pauses execution for the given number of milliseconds
+ * @param {number} millis - duration to pause for in milliseconds
+ * @returns {Promise}
+ */
 async function pause(millis) { return new Promise(resolve => setTimeout(resolve, millis)); }
 
 export const quenchUtils = { pause };
