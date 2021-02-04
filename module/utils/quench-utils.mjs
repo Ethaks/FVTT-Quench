e767d5c5a6d4520eeb72f6344fbce2f27dac3283
Cole Schultz <schultzcole@gmail.com>
2021-02-03 19:21:07 -0600

    Add clearWorld util method

--- module/utils/quench-utils.mjs
+++ module/utils/quench-utils.mjs
@@ -5,4 +5,20 @@
  */
 async function pause(millis) { return new Promise(resolve => setTimeout(resolve, millis)); }
 
-export const quenchUtils = { pause };
+/**
+ * Resets the world to a blank state with no entities.
+ *
+ * WARNING: This will permanently delete every entity in your world (scenes, actors, items, macros, roll tables, journal entries, playlists, chat messages, folders, etc.)
+ * @returns {Promise<void>}
+ */
+async function clearWorld() {
+    const exclude = [User].map(e => e.config.baseEntity.name);
+    for (let collection of Object.values(game)) {
+        if (!(collection instanceof EntityCollection) || exclude.includes(collection.entity)) continue;
+        if (!collection.size) continue;
+
+        await CONFIG[collection.entity].entityClass.delete(collection.entities.map(e => e.id));
+    }
+}
+
+export const quenchUtils = { pause, clearWorld };
