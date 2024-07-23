6ed9d01db2a1949497cbfea43375f492796b053b
Ethaks <ethaks@protonmail.ch>
2024-07-23 15:58:25 +0200

    build: adjust manifest updater for prettier removal

--- tools/manifest-updater.cjs
+++ tools/manifest-updater.cjs
@@ -1,12 +1,8 @@
-const prettier = require("prettier");
-
 module.exports.readVersion = (contents) => JSON.parse(contents).version;
 
 module.exports.writeVersion = (contents, version) => {
 	const json = JSON.parse(contents);
 	json.version = version;
 	json.download = `https://github.com/Ethaks/FVTT-Quench/releases/download/v${version}/module.zip`;
-	return prettier.format(JSON.stringify(json), {
-		parser: "json",
-	});
+	return JSON.stringify(json);
 };
